692aeba146c4c01ce7b1281e626a932df206107d
Frank Rojas <frankrojas215@gmail.com>
2024-09-16 12:51:14 -0500

    more styling and add more components to page

---
+++ app/components/ContactMe/ContactMe.tsx
@@ -0,0 +1,11 @@
+import styles from "./ContactMe.module.css"
+
+export default function ContactMe() {
+    return (
+        <div className={styles.container}>
+            <h1>
+                Contact Me
+            </h1>
+        </div>
+    )
+}
---
+++ app/components/Projects/Projects.tsx
@@ -0,0 +1,11 @@
+import styles from "./Projects.module.css";
+
+export default function Projects() {
+    return (
+        <div className={styles.container}>
+            <h1>
+                Projects
+            </h1>
+        </div>
+    )
+}
