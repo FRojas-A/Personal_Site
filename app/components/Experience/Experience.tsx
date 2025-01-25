dbf4f9c6b149b23cd41b1ec685e12673b7da728f
Frank Rojas <frankrojas215@gmail.com>
2025-01-25 10:45:48 -0600

    remove light theme and add opacity effect for jobs

--- app/components/Experience/Experience.tsx
+++ app/components/Experience/Experience.tsx
@@ -15,38 +15,40 @@ export default function Experience() {
     <SectionWrapper name="experience">
       <section id="experience" className={styles["experience"]}>
         <h1 className={styles["title"]}>Experience</h1>
-        {jobs.map((job, index) => {
-          return (
-            <div key={index} className={styles["job-container"]}>
-              <div className={styles["info"]}>
-                <a
-                  target="_blank"
-                  href={job.urls[0]}
-                  className={styles["link"]}
-                  aria-label={job.company + " (Opens in a new window)"}
-                >
-                  <span></span>
-                  <h4>
-                    {job.title} - {job.company}
-                  </h4>
-                </a>
-                <h5>
-                  {formatDate(job.dateRange[0], dateOptions)} -{" "}
-                  {formatDate(job.dateRange[1], dateOptions)}
-                </h5>
-              </div>
-              <div className={styles["description"]}>
-                <p>{job.description}</p>
-                {}
-                <div className={styles["skills"]}>
-                  {job.skills.map((skill, index) => {
-                    return <SharedPill text={skill} key={index} />;
-                  })}
+        <div className={styles["jobs-list"]}>
+          {jobs.map((job, index) => {
+            return (
+              <div key={index} className={styles["job-container"]}>
+                <div className={styles["info"]}>
+                  <a
+                    target="_blank"
+                    href={job.urls[0]}
+                    className={styles["link"]}
+                    aria-label={job.company + " (Opens in a new window)"}
+                  >
+                    <span></span>
+                    <h4>
+                      {job.title} - {job.company}
+                    </h4>
+                  </a>
+                  <h5>
+                    {formatDate(job.dateRange[0], dateOptions)} -{" "}
+                    {formatDate(job.dateRange[1], dateOptions)}
+                  </h5>
+                </div>
+                <div className={styles["description"]}>
+                  <p>{job.description}</p>
+                  {}
+                  <div className={styles["skills"]}>
+                    {job.skills.map((skill, index) => {
+                      return <SharedPill text={skill} key={index} />;
+                    })}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </section>
     </SectionWrapper>
   );
